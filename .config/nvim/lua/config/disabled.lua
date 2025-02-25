1a1a1442424efa28fb93f51bc0485d76bc89055e
deftcrab8 <conner@elliscontracting.com>
2025-02-24 17:19:37 -0800

    added oil and spider for files and snake case support

---
+++ .config/nvim/lua/config/disabled.lua
@@ -0,0 +1,6 @@
+-- Add disabled plugins here
+--
+return {
+    { "folke/noice.nvim", enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+}
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,26 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any
-local function my_on_attach(bufnr)
-    local api = require("nvim-tree.api")
-
-    local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
-
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
-
-    -- custom mappings
-    vim.keymap.set("n", "k", api.tree.change_root_to_parent, opts("Up"))
-    vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-    vim.keymap.set("n", "l", api.node.navigate.sibling.next, opts("Next Sibling"))
-    vim.keymap.set("n", "h", api.node.navigate.sibling.prev, opts("Previous Sibling"))
-end
-
--- pass to setup along with your other options
-require("nvim-tree").setup({
-    ---
-    on_attach = my_on_attach,
-    ---
-})
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
